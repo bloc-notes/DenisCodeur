366232554bdd0e0717807abd46f7edceefcd49c3
thebigguy270 <37880246+thebigguy270@users.noreply.github.com>
2018-04-06 13:44:14 -0400

    Add files via upload
    Contient l'option 3

---
+++ projet-final/table.php
@@ -0,0 +1,32 @@
+<body>
+    <title>Assigner les privilèges d'accès au professeur</title>
+
+<table>
+<tr>
+<th>Nom d'utilisateur /Cours session</th>
+<th>A-2017</th>
+<th>H-2018</th>
+<th>A-2018</th>
+</tr>
+<tr>
+<td>Lussier, Marc-Antoine</td>
+<td><input type="checkbox" name="cb1_1" value="cb1_1"></td>
+<td><input type="checkbox" name="cb1_2" value="cb1_2"></td>
+<td><input type="checkbox" name="cb1_3" value="cb1_3"></td>
+</tr>
+<tr>
+<td>Hassan Guelleh, Mohamed</td>
+<td><input type="checkbox" name="cb2_1" value="cb2_1"></td>
+<td><input type="checkbox" name="cb2_2" value="cb2_2"></td>
+<td><input type="checkbox" name="cb2_3" value="cb2_3"></td>
+</tr>
+<tr>
+<td>Doyon, Philippe</td>
+<td><input type="checkbox" name="cb3_1" value="cb3_1"></td>
+<td><input type="checkbox" name="cb3_2" value="cb3_2"></td>
+<td><input type="checkbox" name="cb3_3" value="cb3_3"></td>
+</tr>
+</table>
+<input type="submit" value="Enregistrement">
+<input type="submit" value="Retour">
+</body>
