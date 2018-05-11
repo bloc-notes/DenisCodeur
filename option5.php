83bbd1fbde96edf35a60f8144d59aa62da03f726
thebigguy270 <37880246+thebigguy270@users.noreply.github.com>
2018-05-11 12:51:15 -0400

    Add files via upload

--- option5.php
+++ option5.php
@@ -3,7 +3,40 @@ session_start();
 $strNomUtilisateur = $_SESSION["nomComplet"];
 
 require_once "en-tete.php";
+require_once "classe-mysql-2018-03-30-Doyon.php";
+require_once "librairies-communes-2018-03-20-Doyon.php";
+require_once "classe-fichier-08-05-2018-Doyon.php";
 $intDocsEffaces=0;
+$strNomBD = "pjf_microvox";
+$strMonIP = "";
+$strIPServeur = "";
+$strNomServeur = "";
+$strInfosSensibles = "";
+detecteServeur($strMonIP, $strIPServeur, $strNomServeur, $strInfosSensibles);
+$BD = new mysql($strNomBD, $strInfosSensibles);
+$strBtnRetour = "btnRetour";
+$strBtnSuppr = "btnSupprimer";
+$binSuppression = false;
+
+
+
+if(isset($_POST['Action'])){
+    if($_POST['Action']=='Supprimer'){
+        if(isset($_POST['case'])){
+        $binSuppression=true;
+        
+        
+        }
+    }
+    elseif ($_POST['Action']=='Annuler') {
+         header("Location: menuAdmin.php");
+         exit();
+    }
+    elseif ($_POST['Action']=='Retour') {
+         header("Location: menuAdmin.php");
+         exit();
+    }
+}
 ?>
 <script language="JavaScript">
 function cochetout(source) {
@@ -19,9 +52,7 @@ function supprimer() {
 	document.getElementById('etatDoc'+(i+1)).textContent="Supprimé";
 	}
   }
-  document.getElementById("docsEtatApresSuppression").style.display = 'block';
-    document.getElementById("Resultats").style.display = 'block';
-  document.getElementById("arboDocASupprimer").style.display = 'none';
+
 }
 
 function triTable(n) {
@@ -81,8 +112,10 @@ function triTable(n) {
     <header>
         <h1>Arborescence des Documents</h1>
     </header>
-
+<?php if($BD->selectionneEnregistrements("document","C=affichage=0")>0){
+    if($binSuppression==false){?>
 <div id="arboDocASupprimer">
+    <form id="frmSaisie" method="post" action="">
     <table id="tableDocPossiblementEffaces">
         <tr>
         <th onclick="triTable(0)">#</th>
@@ -93,32 +126,41 @@ function triTable(n) {
         <th onclick="triTable(5)">Titre du document</th>
         <th><input type="checkbox" onClick="cochetout(this)" /></th>
         </tr>
+        
+        <?php
+        $arrayUtilisateurs = [];
+        for($i=0;$i<$BD->selectionneEnregistrements("utilisateur","C=statutAdmin=1");$i++){
+            array_push($arrayUtilisateurs,$BD->contenuChamp($i, "nomUtil"));
+        }        
+        for($i=0;$i<$BD->selectionneEnregistrements("document","C=affichage=0","T=\"sessionDoc\", \"sigleCoursDoc\", \"titre\"");$i++){
+        ?>
         <tr>
-            <td>1</td>
-            <td>H-2017</td>
-            <td>420-3B4</td>
-            <td>Brousseau, Louis-Marie</td>
-            <td>2017-11-15</td>
-            <td>Fichier à copier sur P</td>
-            <td><input type="checkbox" name="case" value="case1"></td>
+            <td><?php echo $i+1; ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sessionDoc"); ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sigleCoursDoc"); ?></td>
+            <td><?php echo $BD->contenuChamp($i,"ajoutePar"); ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sigleCoursDoc"); ?></td>
+            <td><?php echo $BD->contenuChamp($i, "titre"); ?></td>
+            <td><input type="checkbox" name="case" value="<?php echo ($i+1); ?>"></td>
         </tr>
-        <tr>
-            <td>2</td>
-            <td>H-2018</td>
-            <td>420-4B4</td>
-            <td>Cherief, Ferroudja</td>
-            <td>2018-03-01</td>
-            <td>SQL</td>
-            <td><input type="checkbox" name="case" value="case2"></td>
-        </tr>
+        <?php } ?>
     </table>
-    
-    <button id="Action" name="Action" type="button" value="Supprimer" onclick='supprimer();'>Supprimer</button>
+    <br/>
+    <button id="Action" name="Action" type="submit" value="Supprimer" onclick='supprimer();'>Supprimer</button>
     <button id="Action" name="Action" type="button" value="Annuler" >Annuler</button>
+</form>
+    
 </div>
-
+<?php } else {
+    var_dump($_POST);
+    $intDocsEffaces = count($_POST['case']);
+        
+    
+    ?>
 <div id="docsEtatApresSuppression">
 <table id="tableDocsApresTraitement">
+
+
         <tr>
         <th>#</th>
         <th>Session</th>
@@ -128,45 +170,102 @@ function triTable(n) {
         <th>Titre du document</th>
         <th>Verdict</th>
         </tr>
+        <?php for($i=0;$i<$BD->selectionneEnregistrements("document","C=affichage=0","T=\"sessionDoc\", \"sigleCoursDoc\", \"titre\"");$i++){
+        ?>
         <tr>
-            <td>1</td>
-            <td>H-2017</td>
-            <td>420-3B4</td>
-            <td>Brousseau, Louis-Marie</td>
-            <td>2017-11-15</td>
-            <td>Fichier à copier sur P</td>
-            <td><span id="etatDoc1"></span></td>
+            <td><?php echo $i+1 ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sessionDoc") ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sigleCoursDoc"); ?></td>
+            <td><?php echo $BD->contenuChamp($i,"ajoutePar"); ?></td>
+            <td><?php echo $BD->contenuChamp($i, "sigleCoursDoc"); ?></td>
+            <td><?php echo $BD->contenuChamp($i, "titre"); ?></td>
+            
+            <td><span id="<?php echo "etatDoc".($i+1) ?>"></span></td>
         </tr>
-        <tr>
-            <td>2</td>
-            <td>H-2018</td>
-            <td>420-4B4</td>
-            <td>Cherief, Ferroudja</td>
-            <td>2018-03-01</td>
-            <td>SQL</td>
-            <td><span id="etatDoc2"></span></td>
-        </tr>
+        <?php
+        
+        
+        
+        }
+        $intDocsPreserves = $BD->selectionneEnregistrements("document","C=affichage=0","T=\"sessionDoc\", \"sigleCoursDoc\", \"titre\"");
+        for($i=0;$i<count($_POST['case']);$i++){
+            
+            for($j=0;$j<$BD->selectionneEnregistrements("document","C=affichage=0","T=\"sessionDoc\", \"sigleCoursDoc\", \"titre\"");$j++){
+            if($_POST['case'][$i] == $j+1){
+               echo "<script> document.getElementById('etatDoc". ($j+1). "').textContent='Supprimé'; </script>";
+               $BD->supprimeEnregistrements("document","titre='".$BD->contenuChamp($j, "titre").
+                       "' AND sigleCoursDoc='".$BD->contenuChamp($j, "sigleCoursDoc").
+                       "' AND sessionDoc='". $BD->contenuChamp($j, "sessionDoc")."'");
+            }
+            }
+        }
+        ?>
+        
     </table>
 </div>
 <div id="Resultats">
+    
+    
+    
+    
+    
     <table id="tableResultats">
+          <?php
+    //Effacer les documents orphelins
+     
+      
+      
+      $arDocs = array_slice(scandir('donnees/documents/'),2);
+      
+      
+      $compteDocsAvant = count($arDocs);
+      //Array docs dans sql
+      $arrayDocSQL = [];
+      $docsSupprimes = 0;
+        for($i=0;$i<$BD->selectionneEnregistrements("document","T=\"hyperLien\"");$i++){
+          array_push($arrayDocSQL,$BD->contenuChamp($i, "hyperLien"));
+        }   
+      
+      
+      ?>
      <th>No</th> 
      <th>Nom</th> 
      <th>Verdict</th>
+     <?php for($i=0;$i<$compteDocsAvant;$i++){
+         $fic = new fichier('donnees/documents/'.$arDocs[$i]);
+         
+         ?>
      <tr>
-     <td>1</td>
-     <td>SQL2</td>
-     <td>Effacé</td>
+     <td><?php echo $i+1 ?></td>
+     <td><?php echo $arDocs[$i]; ?></td>
+     <td><?php if(!in_array($arDocs[$i],$arrayDocSQL)){ ?>
+     Supprimé
+     <?php
+     $fic->supprime();
+     $docsSupprimes++;
+     } else{} ?>
+     </td>
+     
      </tr>
+      <?php
+     }
+    //Effacer les documents orphelins ?>
     </table>
-    Documents effacés:<?php echo $intDocsEffaces ?>
+    Documents effacés:<?php echo $docsSupprimes ?>
+    <br/>
+    Documents conservés:<?php echo $compteDocsAvant - $docsSupprimes ?>
 </div>
+<?php } ?>
+    
+    <form method="post">
+    <button id="Action" name="Action" type="submit" value="Retour" >Retour</button>
+    </form> 
+    
 </div>
-    <script type="text/javascript">
-    var div1 = document.getElementById("docsEtatApresSuppression");
-    div1.style.display = 'none';
-    document.getElementById("Resultats").style.display = 'none';
-    </script>
-<?php
+
+    <?php } else{ ?>
+<h1>Il n'y a pas de documents à effacer</h1>
+<?php } 
+
 require_once "pied-page.php";
 
